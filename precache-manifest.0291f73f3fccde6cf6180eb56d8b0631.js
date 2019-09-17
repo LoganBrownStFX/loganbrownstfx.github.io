2678ee9e1ce7d104d885dfac886573705bb304da
Logan Brown <loganbrowncpa@gmail.com>
2019-09-17 19:19:58 -0300

    Updates

---
+++ precache-manifest.0291f73f3fccde6cf6180eb56d8b0631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8355e23b23c9acaa6bd1c701096eddd7",
+    "url": "/loganbrownstfx.github.io/index.html"
+  },
+  {
+    "revision": "cc10dc629c3cefc8c122",
+    "url": "/loganbrownstfx.github.io/static/css/main.5bff546b.chunk.css"
+  },
+  {
+    "revision": "99f34a52a153d65b6e46",
+    "url": "/loganbrownstfx.github.io/static/js/2.19656c09.chunk.js"
+  },
+  {
+    "revision": "cc10dc629c3cefc8c122",
+    "url": "/loganbrownstfx.github.io/static/js/main.3b15367d.chunk.js"
+  },
+  {
+    "revision": "84dbce76a7e1241e3d11",
+    "url": "/loganbrownstfx.github.io/static/js/runtime~main.fc6908bd.js"
+  }
+]);
